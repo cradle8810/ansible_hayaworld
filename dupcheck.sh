e91ee6674e9b2182ab3f1db08811e23bc6c4eb69
cradle8810 <falcon.int50m@gmail.com>
2020-12-24 20:56:09 +0900

    Add Domain Duplication Validation Mechanism

---
+++ dupcheck.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+  echo "Usage $0 <files>"
+  exit 2
+fi
+
+cat "$@" | \
+sort | uniq -c | sort | \
+grep -E -v '^(\s)+1' > /tmp/result
+
+duplines=$(wc -l /tmp/result | awk '{print $1}')
+
+if [ "$duplines" -eq 0 ]; then
+  echo "[ OK ] No Duplicate lines."
+  exit 0
+else
+  echo "[ NG ] Duplicate Line(s) found."
+  cat /tmp/result
+  exit 1
+fi
