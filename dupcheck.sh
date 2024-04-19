eb15ebacdcceab403df90b6da422f1bd277c9170
Hayato <falcon.int50m@gmail.com>
2024-04-19 18:40:57 +0900

    Add check mechanism to exist files on dupcheck.sh
    This actions must be failed

--- dupcheck.sh
+++ dupcheck.sh
@@ -5,6 +5,14 @@ if [ "$#" -eq 0 ]; then
   exit 2
 fi
 
+for file in "$@"
+do
+  if [ ! -f ${file} ]; then
+    echo "[ NG ] No ${file} exist." > /dev/stderr
+    exit 3
+  fi
+done
+
 cat "$@" | \
 sort | uniq -c | sort | \
 grep -E -v '^(\s)+1 ' > /tmp/result
