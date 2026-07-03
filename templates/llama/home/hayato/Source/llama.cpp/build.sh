1617ba2592aa69d60fdde73a0c19020cee6aea59
Hayato <falcon.int50m@gmail.com>
2026-07-04 06:57:23 +0900

    Fix script error

--- templates/llama/home/hayato/Source/llama.cpp/build.sh
+++ templates/llama/home/hayato/Source/llama.cpp/build.sh
@@ -2,13 +2,14 @@
 
 #rm -fr build
 
-VULKAN_SDK=/opt/vulkan/x86_64 cmake -B build \
+export VULKAN_SDK=/opt/vulkan/x86_64 
+
+cmake -B build \
   -DGGML_VULKAN=1 \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_CXX_FLAGS=-fPIC \
   -DCMAKE_C_FLAGS=-fPIC \
-#  -DLLAMA_CCACHE=OFF \
-  -DVulkan_LIBRARY=/opt/vulkan/x86_64/lib/VulkanLoader/lib/libvulkan.solibvulkan.so \
+  -DVulkan_LIBRARY=/opt/vulkan/x86_64/lib/VulkanLoader/lib/libvulkan.so \
   -DVulkan_INCLUDE_DIR=/opt/vulkan/x86_64/include
 
 cmake --build build --config Release -j $(nproc)
