02f0c3fe20f7fd0798d94e03807553cc0868e90d
Hayato <falcon.int50m@gmail.com>
2026-07-04 06:53:15 +0900

    Add tag 'llama' to build task and fix build.sh

--- templates/llama/home/hayato/Source/llama.cpp/build.sh
+++ templates/llama/home/hayato/Source/llama.cpp/build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+#rm -fr build
+
 VULKAN_SDK=/opt/vulkan/x86_64 cmake -B build \
   -DGGML_VULKAN=1 \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_CXX_FLAGS=-fPIC \
   -DCMAKE_C_FLAGS=-fPIC \
-  -DLLAMA_CCACHE=OFF \
+#  -DLLAMA_CCACHE=OFF \
   -DVulkan_LIBRARY=/opt/vulkan/x86_64/lib/VulkanLoader/lib/libvulkan.solibvulkan.so \
   -DVulkan_INCLUDE_DIR=/opt/vulkan/x86_64/include
 
 cmake --build build --config Release -j $(nproc)
+
+cd build/
+sudo make install
