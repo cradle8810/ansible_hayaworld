a95b067a4c56370ac1d41c975fd713775d06c96b
Hayato <falcon.int50m@gmail.com>
2026-07-04 06:02:23 +0900

    Add build script for llama

---
+++ templates/llama/home/hayato/Source/llama.cpp/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VULKAN_SDK=/opt/vulkan/x86_64 cmake -B build \
+  -DGGML_VULKAN=1 \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_CXX_FLAGS=-fPIC \
+  -DCMAKE_C_FLAGS=-fPIC \
+  -DLLAMA_CCACHE=OFF \
+  -DVulkan_LIBRARY=/opt/vulkan/x86_64/lib/VulkanLoader/lib/libvulkan.solibvulkan.so \
+  -DVulkan_INCLUDE_DIR=/opt/vulkan/x86_64/include
+
+cmake --build build --config Release
